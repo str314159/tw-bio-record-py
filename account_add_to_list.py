2b9488aa3dbe1a59462a5876d972de1dda6c1509
str314159 <satoru0099@gmail.com>
2022-09-26 19:58:32 +0900

    #6 openのエラー時にinputでデータをリセットするか判定する

---
+++ account_add_to_list.py
@@ -0,0 +1,11 @@
+import json
+
+username = input("username:")
+
+list_open = open("list.json", "r")
+list_load = json.load(list_open)
+list_load["data"].append({"username":username})
+f = open("list.json", "w")
+json.dump(list_load,f,indent=4,ensure_ascii=False)
+list_open.close
+f.close
